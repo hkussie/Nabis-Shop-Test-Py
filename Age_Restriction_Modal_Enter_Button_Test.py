ce09391b04c4226f097b4bafa6bdd0e55107e12b
Harrison Kussie <hkussie@gmail.com>
2021-11-01 16:12:20 -0700

    Add age restriction modal enter button test
    Add age restriction modal enter button test to test suite

--- Age_Restriction_Modal_Enter_Button_Test.py
+++ Age_Restriction_Modal_Enter_Button_Test.py
@@ -1,3 +1,4 @@
+import time
 import unittest 
 from selenium import webdriver
 import chromedriver_binary
@@ -12,16 +13,14 @@ class TestAgeRestrictionRedirectionModal(unittest.TestCase):
 	def setUp(self):
 		self.driver = webdriver.Chrome()
 
-	# Navigate to the age restriction modal
+	# Navigate to the age restriction modal, hit enter button and then scroll down to the bottom of the page
 	def test_age_modal_redirection(self):
 		driver = self.driver 
 		driver.get("http://shop.nabis.com/")
 		driver.implicitly_wait(10)
-		driver.find_element_by_xpath('/html/body/div[3]/div/div[3]/a/span/button').click()
-		self.redirectURL = driver.current_url
-		self.expectedURL = "https://www.nabis.com/"
-		self.assertEqual(self.redirectURL, self.expectedURL)
-
+		driver.find_element_by_xpath('/html/body/div[3]/div/div[3]/span/button').click()
+		driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+		
 	# Close the browser
 	def tearDown(self):
 		self.driver.close()
